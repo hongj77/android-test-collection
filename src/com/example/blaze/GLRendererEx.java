f59a7e0a0e6620f71c5e420e3850fc4004f53bf5
Hong <hongj77@gmail.com>
2014-08-21 12:46:35 -0400

    i hate openGL

---
+++ src/com/example/blaze/GLRendererEx.java
@@ -0,0 +1,52 @@
+package com.example.blaze;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.GLU;
+import android.opengl.GLSurfaceView.Renderer;
+
+public class GLRendererEx implements Renderer {
+
+	private GLTriangleEx tri;
+	
+	public GLRendererEx() {
+		tri = new GLTriangleEx();
+	}
+	
+	
+	@Override
+	public void onSurfaceCreated(GL10 gl, EGLConfig eglConfig) {
+		// TODO Auto-generated method stub
+		gl.glDisable(GL10.GL_DITHER);
+		gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_FASTEST);
+		gl.glClearColor(.8f, 0, .2f, 1);
+		gl.glClearDepthf(1f);
+	}
+	
+	@Override
+	public void onDrawFrame(GL10 gl) {
+		// TODO Auto-generated method stub
+		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+		
+		gl.glMatrixMode(GL10.GL_MODELVIEW);
+		gl.glLoadIdentity();
+		GLU.gluLookAt(gl, 0, 0, -5, 0, 0, 0, 0, 2, 0);
+		
+		tri.draw(gl);
+	}
+
+	@Override
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		// TODO Auto-generated method stub
+		gl.glViewport(0, 0, width, height);
+		float ratio = (float) width/height;
+		gl.glMatrixMode(GL10.GL_PROJECTION);
+		gl.glLoadIdentity();
+		gl.glFrustumf(-ratio, ratio, -1, 1, 1, 25);
+		
+	}
+
+	
+
+}
